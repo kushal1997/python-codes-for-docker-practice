88a4002f6d83bcad0d25c1a5662fa59fc31b1ed1
kushal1997 <kushalrao103@gmail.com>
2024-10-27 18:28:57 +0530

    pragram 2 as name shaving

---
+++ name_saving/myapp.py
@@ -0,0 +1,17 @@
+user_name=input("Enter your name to store in file or press enter : ")
+
+if user_name:
+    with open('user_info.txt','a') as file:
+        file.write(user_name + "\n")
+
+show_info = input ("Do you want to see all the names ? y / n : ")
+
+if show_info == 'y':
+    try:
+        with open('user_info.txt','r') as file:
+            content = file.readlines()
+    except Exception as e:
+        print(e,type(e))
+    else:
+        for line in content:
+            print(f"{line.rstrip()}")
